04898bb80a80a2d6854c4f8c0b2b422f570fe81c
Jenna Salau <jenna.salau@gmail.com>
2017-05-20 23:12:09 +1000

    Refactored and optimised the observer logic.. no longer shows warnings and faster

--- src/Bootstrapper.js
+++ src/Bootstrapper.js
@@ -12,18 +12,29 @@ import Logger from './Logger';
 const DEFAULT_HABITAT_SELECTOR = 'data-component';
 
 /**
- * Parses a container and populate components
- * @param {array}     container             The container
- * @param {array}     elements              The elements to parse
- * @param {string}    componentSelector     The component selector
- * @param cb
+ * Safe callback wrapper
+ * @param {null|function}		cb			- The callback
+ * @private
  */
-function parseContainer(container, elements, componentSelector, cb = null) {
-	console.log('Running Parser ---- ');
+function _callback(cb) {
+	if (typeof cb === 'function') {
+		cb();
+	}
+}
+
+/**
+ * Apply a container to nodes and populate components
+ * @param {array}     container             The container
+ * @param {array}     nodes              	The elements to parse
+ * @param {string}    componentSelector     The component selector
+ * @param {function}  [cb=null]   			- Optional callback
+ * @private
+ */
+function _applyContainer(container, nodes, componentSelector, cb = null) {
 
 	// Bail out early if no elements to parse
-	if (!elements || !elements.length) {
-		if (typeof cb === 'function') { cb(); }
+	if (!nodes || !nodes.length) {
+		_callback(cb);
 		return;
 	}
 
@@ -31,32 +42,35 @@ function parseContainer(container, elements, componentSelector, cb = null) {
 	const id = container.id();
 
 	// Iterate over component elements in the dom
-	for (let i = 0; i < elements.length; ++i) {
-		const ele = elements[i];
-		if (!Habitat.hasHabitat(ele)) {
-			const componentName = ele.getAttribute(componentSelector);
-			const component = container.resolve(componentName);
-			if (component) {
-				if (process.env.NODE_ENV !== 'production') {
-					// Expensive operation, only do on non prod builds
-					if (ele.querySelector(`[${componentSelector}]`)) {
-						Logger.warn('RHW08', 'Component should not contain any nested components.', ele);
-					}
+	for (let i = 0; i < nodes.length; ++i) {
+		const ele = nodes[i];
+
+		// Ignore elements that have already been connected
+		if (Habitat.hasHabitat(ele)) {
+			continue;
+		}
+
+		const componentName = ele.getAttribute(componentSelector);
+		const component = container.resolve(componentName);
+		if (component) {
+			// Expensive operation, only do on non prod builds
+			if (process.env.NODE_ENV !== 'production') {
+				if (ele.querySelector(`[${componentSelector}]`)) {
+					Logger.warn('RHW08', 'Component should not contain any nested components.', ele);
 				}
-				factory.inject(
-					component,
-					Habitat.parseProps(ele),
-					Habitat.create(ele, id));
-			} else {
-				Logger.error('RHW01', `Cannot resolve component "${componentName}" for element.`, ele);
 			}
+
+			// Inject the component
+			factory.inject(
+				component,
+				Habitat.parseProps(ele),
+				Habitat.create(ele, id));
+		} else {
+			Logger.error('RHW01', `Cannot resolve component "${componentName}" for element.`, ele);
 		}
 	}
 
-
-	if (typeof cb === 'function') {
-		cb();
-	}
+	_callback(cb);
 }
 
 /**
@@ -73,14 +87,31 @@ export default class Bootstrapper {
 			throw new Error('React Habitat requires a window but cannot see one :(');
 		}
 
-		// Set dom component selector
+		/**
+		 * The DOM component selector
+		 * @type {string}
+		 */
 		this.componentSelector = DEFAULT_HABITAT_SELECTOR;
 
-		// The container
+		/**
+		 * If true, the container will be applied to dom mutations automatically. True by default.
+		 * i.e update(*addedNodes*)
+		 * @type {boolean}
+		 */
+		this.enableWatcher = true;
+
+		/**
+		 * The container
+		 * @type {Container|null|
+		 * @private
+		 */
 		this._container = null;
 
-		// Dom mutation observer
-		this.enableDomWatcher = true;
+		/**
+		 * The watcher's observer instance or null
+		 * @type {MutationObserver|null}
+		 * @private
+		 */
 		this._observer = null;
 	}
 
@@ -101,55 +132,50 @@ export default class Bootstrapper {
 		this._container = container;
 
 		// Wire up the components from the container
-		parseContainer(
-			this._container,
-			window.document.body.querySelectorAll(`[${this.componentSelector}]`),
-			this.componentSelector,
-			() => {
-				// Start the dom watcher unless disabled
-				if (this.enableDomWatcher) {
-					console.log('starting watcher');
-					this.startDomWatcher();
-				}
-
-				// Callback
-				if (typeof cb === 'function') { cb(); }
-			}
-		);
+		this.update(null, () => {
+			_callback(cb);
+		});
 	}
 
 	/**
 	* Apply the container to an updated dom structure
 	* This should be triggered anytime HTML has been ajaxed in
 	* @param {node}		node		- Target node to parse or null for entire document body
-	* @param {function}  	[cb=null]   - Optional callback
+	* @param {function}		[cb=null]	- Optional callback
 	*/
-	domDidUpdate(node, cb = null) {
-		if (this._container === null) {
+	update(node, cb = null) {
+		// Check if we have a container before attempting an update
+		if (!this._container) {
+			_callback(cb);
 			return;
 		}
 
+		_callback(this.domWillUpdate);
+
 		// Temporarily stop the watcher from triggering due to Habitat injections
 		// Note: This could possibly miss some dom changes during parse time.. bug maybe?
-		this.stopDomWatcher();
+		this.stopWatcher();
 
-		parseContainer(
+		_applyContainer(
 			this._container,
 			node || window.document.body.querySelectorAll(`[${this.componentSelector}]`),
 			this.componentSelector,
 			() => {
 				// Restart the dom watcher unless disabled
-				if (this.enableDomWatcher) {
-					this.startDomWatcher();
+				if (this.enableWatcher) {
+					this.startWatcher();
 				}
 
-				// Callback
-				if (typeof cb === 'function') { cb(); }
+				_callback(cb);
+				_callback(this.domDidUpdate);
 			}
 		);
 	}
 
-	startDomWatcher(node) {
+	/**
+	 * Start DOM watcher for auto wire ups
+	 */
+	startWatcher() {
 		// Feature available?
 		if (typeof MutationObserver === 'undefined') {
 			Logger.warn('RHWXX', 'MutationObserver not available');
@@ -162,16 +188,21 @@ export default class Bootstrapper {
 		}
 
 		// Start observing for dom changes filtered by our component selector
-		this._observer.observe(node || window.document.body, {
+		this._observer.observe(window.document.body, {
 			childList: true,
 			attributes: true,
 			subtree: true,
+			attributeOldValue: false,
+			characterData: false,
+			characterDataOldValue: false,
 			attributeFilter: [this.componentSelector],
 		});
-
 	}
 
-	stopDomWatcher() {
+	/**
+	 * Stop the DOM watcher if running
+	 */
+	stopWatcher() {
 		if (this._observer && this._observer.disconnect) {
 			this._observer.disconnect();
 		}
@@ -183,11 +214,14 @@ export default class Bootstrapper {
 	*/
 	_handleDomMutation(mutationRecord) {
 		if (typeof mutationRecord !== 'undefined') {
-			this.domDidUpdate(mutationRecord.addedNodes);
+			let diff = mutationRecord.filter(r => r.addedNodes.length);
+			// Only run update if nodes have been added
+			if (diff.length) {
+				this.update();
+			}
 		} else {
-			// Polyfill Fallback
-			console.log('Mutation fallback');
-			this.domDidUpdate();
+			// Fallback
+			this.update();
 		}
 	}
 
@@ -196,6 +230,9 @@ export default class Bootstrapper {
 	 * @param {function}	[cb=null]	- Optional callback
 	 */
 	dispose(cb = null) {
+
+		// Stop dom watcher if any
+		this.stopWatcher();
 
 		// get the container's factory
 		const factory = this._container.domFactory();
@@ -212,17 +249,12 @@ export default class Bootstrapper {
 			Habitat.destroy(habitats[i]);
 		}
 
-		// Stop dom watcher if any
-		this.stopDomWatcher();
-
 		// Reset and release
 		this._container = null;
 		this._elements = null;
 		this._observer = null;
 
 		// Handle callback
-		if (typeof cb === 'function') {
-			cb.call();
-		}
+		_callback(cb, this);
 	}
 }
