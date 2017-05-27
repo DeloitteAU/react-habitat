63e30869a62a0448d2a7a4b04efba5e2d6d87063
Salau, Jenna (AU - Melbourne) <jsalau@deloitte.com.au>
2017-05-27 22:39:12 +1000

    Updating travis config

--- karma.conf.js
+++ karma.conf.js
@@ -1,8 +1,15 @@
 module.exports = function(config) {
 	const configuration = {
+		// base path that will be used to resolve all patterns (eg. files, exclude)
 		basePath: './',
+
+		// frameworks to use
+		// available frameworks: https://npmjs.org/browse/keyword/karma-adapter
 		frameworks: ['jasmine'],
+
 		//polyfill: ['Promise', 'Object.assign', 'MutationObserver'],
+
+		// list of files / patterns to load in the browser
 		files: [
 			'tests/**/*.spec.js'
 		],
@@ -13,6 +20,23 @@ module.exports = function(config) {
 				flags: ['--no-sandbox']
 			}
 		},
+
+		// start these browsers
+		// available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
+		browsers: ['Chrome'],
+
+		// web server port
+		port: 9876,
+
+		// enable / disable colors in the output (reporters and logs)
+		colors: true,
+
+		// level of logging
+		// possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+		logLevel: config.LOG_INFO,
+
+		// enable / disable watching file and executing tests whenever any file changes
+		autoWatch: false,
 
 		preprocessors: {
 			// add webpack as preprocessor
@@ -52,14 +76,16 @@ module.exports = function(config) {
 			// webpack-dev-middleware configuration
 			// i. e.
 			stats: 'errors-only'
-		}
+		},
+
+		// Continuous Integration mode
+		// if true, Karma captures browsers, runs the tests and exits
+		singleRun: false
 	};
 
-	// if (process.env.TRAVIS) {
-	// 	configuration.browsers = ['Chrome_travis_ci'];
-	// } else {
-	// 	configuration.browsers = ['Chrome'];
-	// }
+	if (process.env.TRAVIS) {
+	 	configuration.browsers = ['Chrome_travis_ci'];
+	}
 
 	config.set(configuration);
 };
